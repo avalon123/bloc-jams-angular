56763b315974a1693715695c301185eab271f2e6
avalon123 <bzhu9035@gmail.com>
2015-10-29 18:07:42 -0700

    checkpoint3 for angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-var blocJames = angular.module('blocJams', []);
+var blocJames = angular.module('blocJams', ['ui.router']);
+
+blocJames.config(function($stateProvider, $locationProvider) {
+
+     $locationProvider.html5Mode({
+         enabled: true,
+         requireBase: false
+     });
+    
+     $stateProvider.state('landing', {
+         url: '/landing',
+         controller: 'landing.controller',
+         templateUrl: '/templates/landing.html'
+     });
+     
+     $stateProvider.state('album', {
+         url: '/album',
+         controller: 'Album.controller',
+         templateUrl: '/templates/album.html'
+     });
+     
+     $stateProvider.state('collection', {
+         url: '/collection',
+         controller: 'collection.controller',
+         templateUrl: '/templates/collection.html'
+     });
+
+ });
+
